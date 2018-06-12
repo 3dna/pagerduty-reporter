5fb2aab9169f771d425397f40ba9cd8618908f6b
Ryan Morten <awesinine@users.noreply.github.com>
2018-06-12 14:36:39 -0700

    Update schema_setup.sh

--- postgres/schema_setup.sh
+++ postgres/schema_setup.sh
@@ -1,2 +1,4 @@
+## psql required to get this going. its easier to go to hostname:8080 and use the web interface to get the sql in if you dont have psql installed locally IMO
+
 #!/bin/bash
 psql -d pagerduty -U postgres -f schemas.sql
